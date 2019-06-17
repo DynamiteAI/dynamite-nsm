56ac8062fdf1785887cb9afa5a0c466f3f9d0b36
JaminB <jamin@vlabs.io>
2019-06-17 15:11:42 -0400

    updated installer

--- dynamite-installer.py
+++ dynamite-installer.py
@@ -1,16 +1,16 @@
 import sys
 
 from installer import elasticsearch
-
 if __name__ == '__main__':
     if not elasticsearch.is_root():
         sys.stderr.write('[-] This script must be run as root.\n')
         sys.exit(1)
 
-    if not elasticsearch.get_memory_available_bytes() < 5368709120:
-        sys.stderr.write('[-] Dynamite ElasticSearch requires atleast 5GB to run currently available [{} GB]'.format(
+    if elasticsearch.get_memory_available_bytes() < 5 * (1000 **3):
+        sys.stderr.write('[-] Dynamite ElasticSearch requires atleast 5GB to run currently available [{} GB]\n'.format(
             elasticsearch.get_memory_available_bytes()/(1024 ** 3)
         ))
+        sys.exit(1)
 
     es_installer = elasticsearch.ElasticInstaller()
     es_installer.download_java(stdout=True)
