9fedd619152120e884a807d38549d69e2ce47f05
jaminbecker <jamin@vlabs.io>
2019-06-26 13:00:35 -0400

    Updated to 6GB default

--- dynamite-installer.py
+++ dynamite-installer.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
         sys.stderr.write('[-] This script must be run as root.\n')
         sys.exit(1)
 
-    if elasticsearch.get_memory_available_bytes() < 5 * (1000 **3):
-        sys.stderr.write('[-] Dynamite ElasticSearch requires atleast 5GB to run currently available [{} GB]\n'.format(
+    if elasticsearch.get_memory_available_bytes() < 6 * (1000 ** 3):
+        sys.stderr.write('[-] Dynamite ElasticSearch requires at-least 6GB to run currently available [{} GB]\n'.format(
             elasticsearch.get_memory_available_bytes()/(1024 ** 3)
         ))
         sys.exit(1)
