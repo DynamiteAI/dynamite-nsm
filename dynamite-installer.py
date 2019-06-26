ebe4fb8fe34b581bd66ad3ac752620f70ff300db
jaminbecker <jamin@vlabs.io>
2019-06-26 15:58:13 -0400

    added argument parsing to installer

--- dynamite-installer.py
+++ dynamite-installer.py
@@ -1,8 +1,20 @@
 import sys
+import argparse
+import traceback
 
 from installer import elasticsearch
 from installer import utilities
-if __name__ == '__main__':
+
+
+def _parse_cmdline():
+    parser = argparse.ArgumentParser(
+        description='Install/Configure the Dynamite Analysis Framework.'
+    )
+    parser.add_argument('mode', metavar='mode', type=str, help='The install/configure mode. [install-elasticsearch]')
+    return parser.parse_args()
+
+
+def install_elasticsearch():
     if not utilities.is_root():
         sys.stderr.write('[-] This script must be run as root.\n')
         sys.exit(1)
@@ -12,12 +24,24 @@ if __name__ == '__main__':
             utilities.get_memory_available_bytes()/(1024 ** 3)
         ))
         sys.exit(1)
+    try:
+        es_installer = elasticsearch.ElasticInstaller()
+        es_installer.download_java(stdout=True)
+        es_installer.extract_java(stdout=True)
+        es_installer.setup_java()
+        utilities.create_dynamite_user('password')
+        es_installer.download_elasticsearch(stdout=True)
+        es_installer.extract_elasticsearch(stdout=True)
+        es_installer.setup_elasticsearch(stdout=True)
+    except Exception:
+        sys.stderr.write('[-] A fatal error occurred while attempting to install ElasticSearch: ')
+        traceback.print_exc(file=sys.stderr)
+        sys.exit(1)
+    sys.stdout.write('[+] *** ElasticSearch installed successfully. ***\n')
+    sys.exit(0)
 
-    es_installer = elasticsearch.ElasticInstaller()
-    es_installer.download_java(stdout=True)
-    es_installer.extract_java(stdout=True)
-    es_installer.setup_java()
-    utilities.create_dynamite_user('password')
-    es_installer.download_elasticsearch(stdout=True)
-    es_installer.extract_elasticsearch(stdout=True)
-    es_installer.setup_elasticsearch(stdout=True)
+
+if __name__ == '__main__':
+    args = _parse_cmdline()
+    if args.mode == 'install-elasticsearch':
+        install_elasticsearch()
