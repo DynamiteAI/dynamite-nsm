e0d0404ab8898bd239dd1ed37b91cfb9a55c8e5c
jaminbecker <jamin@vlabs.io>
2019-07-02 16:39:28 -0400

    added command debugging

--- installer/logstash.py
+++ installer/logstash.py
@@ -349,9 +349,11 @@ class LogstashProcess:
         self.pid = -1
 
         def start_shell_out():
-            subprocess.call('runuser -l dynamite -c "{} {}/bin/logstash '
-                            '--quiet --path.settings={} & echo \$! > /var/run/dynamite/logstash/logstash.pid"'.format(
-                utilities.get_environment_file_str(), self.config.ls_home, self.config.ls_path_conf), shell=True)
+            command = 'runuser -l dynamite -c "{} {}/bin/logstash ' \
+                      '--quiet --path.settings={} & echo \$! > /var/run/dynamite/logstash/logstash.pid"'.format(
+                utilities.get_environment_file_str(), self.config.ls_home, self.config.ls_path_conf)
+            print(command)
+            subprocess.call(command, shell=True)
         if not utilities.check_pid(self.pid):
             Process(target=start_shell_out).start()
         else:
