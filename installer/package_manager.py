a69f74a45e37de2401e4c3499fdc09a74aed2fc5
jaminbecker <jamin@vlabs.io>
2019-07-03 10:08:23 -0400

    added -y flag for yum

--- installer/package_manager.py
+++ installer/package_manager.py
@@ -8,9 +8,9 @@ class OSPackageManager:
 
     @staticmethod
     def detect_package_manager():
-        apt_get_p = subprocess.Popen('apt-get -h', shell=True)
+        apt_get_p = subprocess.Popen('apt-get -h &>/dev/null', shell=True)
         apt_get_p.communicate()
-        yum_p = subprocess.Popen('yum -h', shell=True)
+        yum_p = subprocess.Popen('yum -h &>/dev/null', shell=True)
         yum_p.communicate()
         if apt_get_p.returncode == 0:
             return 'apt-get'
@@ -18,4 +18,7 @@ class OSPackageManager:
             return 'yum'
 
     def install_packages(self, packages):
-        subprocess.call('{} install {}'.format(self.package_manager, ' '.join(packages)), shell=True)
+        flags = ''
+        if self.package_manager == 'yum':
+            flags = '-y'
+        subprocess.call('{} install {} {}'.format(self.package_manager, ' '.join(packages), flags), shell=True)
