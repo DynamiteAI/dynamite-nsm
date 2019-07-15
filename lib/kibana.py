03dbeb65ab9de70a5d5750abedfd02787fac0374
jaminbecker <jamin@vlabs.io>
2019-07-15 14:44:18 -0400

    pushed test

--- lib/kibana.py
+++ lib/kibana.py
@@ -286,6 +286,7 @@ class KibanaProfiler:
             return False
         host = kibana_config.get_server_host()
         port = kibana_config.get_server_port()
+        print(host, port)
         if host.strip() == '0.0.0.0':
             host = 'localhost'
         return utilities.check_socket(host, port)
