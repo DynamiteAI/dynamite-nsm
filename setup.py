c8e60eec0d1ee65efa43c52e49bc0220b5694e80
jaminbecker <jamin@vlabs.io>
2019-08-13 17:02:24 -0400

    added package data

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ setup(
          'ynamite-nsm',
     version='0.0.9',
     packages=['lib', 'lib.services'],
-    include_package_data=True,
     scripts=['scripts/dynamite', 'scripts/python/dynamite.py'],
     url='http://vlabs.io',
     license='',
     author='Jamin Becker',
     author_email='jamin@vlabs.io',
     description='Dynamite-NSM is an network security monitor with an emphasis on very fast deployment, '
-                'minimal configuration, and intuitive management.'
+                'minimal configuration, and intuitive management.',
+    package_data={'': ['default_configs/elasticsearch/elasticsearch.yml',
+                       'default_configs/filebeat/filebeat.yml',
+                       'default_configs/kibana/kibana.yml',
+                       'default_configs/logstash/*.yml',
+                       'mirrors/*'
+                       ]},
+    include_package_data=True,
 )
