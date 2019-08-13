8959af716578452f056ce2f2fc8d48f6b77778fc
jaminbecker <jamin@vlabs.io>
2019-08-13 17:26:30 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ setup(
     author_email='jamin@vlabs.io',
     description='Dynamite-NSM is an network security monitor with an emphasis on very fast deployment, '
                 'minimal configuration, and intuitive management.',
-    package_data={'': ['default_configs/elasticsearch/elasticsearch.yml',
-                       'default_configs/filebeat/filebeat.yml',
-                       'default_configs/kibana/kibana.yml',
-                       'default_configs/logstash/*.yml',
-                       'mirrors/*'
+    package_data={'': ['lib/default_configs/elasticsearch/elasticsearch.yml',
+                       'lib/default_configs/filebeat/filebeat.yml',
+                       'lib/default_configs/kibana/kibana.yml',
+                       'lib/default_configs/logstash/*.yml',
+                       'lib/mirrors/*'
                        ]},
     include_package_data=True,
 )
