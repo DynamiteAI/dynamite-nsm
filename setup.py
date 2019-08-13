9ad3ea43c8ac4b2f32b8cc9b6171388495fd2e59
root <root@ip-172-31-4-31.us-gov-west-1.compute.internal>
2019-08-13 22:23:33 +0000

    fixed some major issues with setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
-from setuptools import setup
+import os
+import shutil
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        try:
+            os.mkdir('/tmp/dynamite')
+        except OSError:
+            pass
+        try:
+            shutil.copytree('mirrors/', '/tmp/dynamite/mirrors')
+            shutil.copytree('default_configs/', '/tmp/dynamite/default_configs')
+        except Exception:
+            print('[-] config directories already exist')
+        install.run(self)
 
 setup(
     name='dls'
-         'ynamite-nsm',
+     'ynamite-nsm',
     version='0.0.9',
-    packages=['lib', 'lib.services'],
+    packages=find_packages(),
     scripts=['scripts/dynamite', 'scripts/python/dynamite.py'],
     url='http://vlabs.io',
     license='',
@@ -12,11 +29,8 @@ setup(
     author_email='jamin@vlabs.io',
     description='Dynamite-NSM is an network security monitor with an emphasis on very fast deployment, '
                 'minimal configuration, and intuitive management.',
-    package_data={'': ['lib/default_configs/elasticsearch/elasticsearch.yml',
-                       'lib/default_configs/filebeat/filebeat.yml',
-                       'lib/default_configs/kibana/kibana.yml',
-                       'lib/default_configs/logstash/*.yml',
-                       'lib/mirrors/*'
-                       ]},
     include_package_data=True,
+    cmdclass={
+        'install': PostInstallCommand
+    }
 )
