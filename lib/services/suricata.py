6196a63133bd6784ccaaec04b4a67beefc139410
jaminbecker <jamin@vlabs.io>
2019-09-06 11:02:54 -0400

    updated suricata install process

--- lib/services/suricata.py
+++ lib/services/suricata.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 import shutil
 import tarfile
 import subprocess
@@ -99,11 +100,11 @@ class SuricataInstaller:
             pf_ring_install.extract_pf_ring(stdout=True)
             pf_ring_install.setup_pf_ring(stdout=True)
         try:
-            os.link(os.path.join(pf_ring_install.install_directory, 'lib', 'libpcap.so'), '/lib/libpcap.so.1')
+            os.symlink(os.path.join(pf_ring_install.install_directory, 'lib', 'libpcap.so'), '/lib/libpcap.so.1')
         except Exception as e:
             sys.stderr.write('[-] Failed to re-link libpcap.so -> /lib/libpcap.so.1: {}\n'.format(e))
         try:
-            os.link(os.path.join(pf_ring_install.install_directory, 'lib', 'libpfring.so'), '/lib/libpfring.so.1')
+            os.symlink(os.path.join(pf_ring_install.install_directory, 'lib', 'libpfring.so'), '/lib/libpfring.so.1')
         except Exception as e:
             sys.stderr.write('[-] Failed to re-link libpfring.so -> /lib/libpfring.so.1: {}\n'.format(e))
         if stdout:
