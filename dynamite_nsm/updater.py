d6ee21288bb5db51848cdf9ce1386491210d7dde
jaminbecker <jamin@vlabs.io>
2019-10-08 10:35:56 -0400

    removed possibly redundant logic, added MIRRORS global

--- dynamite_nsm/updater.py
+++ dynamite_nsm/updater.py
@@ -12,12 +12,11 @@ def update_default_configurations():
     create_dynamite_root_directory()
     download_file('https://github.com/DynamiteAI/dynamite-nsm-configs/raw/master/default_configs.tar.gz',
                   'default_configs.tar.gz', stdout=True)
-    shutil.rmtree('/etc/dynamite/default_configs/', ignore_errors=True)
+    shutil.rmtree(const.DEFAULT_CONFIGS, ignore_errors=True)
     time.sleep(1)
     try:
         print('Copying default_configs -> /etc/dynamite/default_configs')
         extract_archive(os.path.join(const.INSTALL_CACHE, 'default_configs.tar.gz'), '/etc/dynamite/')
-
     except IOError as e:
         sys.stderr.write('[-] An error occurred while attempting to extract file. [{}]\n'.format(e))
 
@@ -26,10 +25,9 @@ def update_mirrors():
     create_dynamite_root_directory()
     download_file('https://github.com/DynamiteAI/dynamite-nsm-configs/raw/master/mirrors.tar.gz',
                   'mirrors.tar.gz', stdout=True)
-    shutil.rmtree('/etc/dynamite/mirrors/', ignore_errors=True)
+    shutil.rmtree(const.MIRRORS, ignore_errors=True)
     try:
         print('Copying mirrors -> /etc/dynamite/mirrors')
         extract_archive(os.path.join(const.INSTALL_CACHE, 'mirrors.tar.gz'), '/etc/dynamite/')
-        shutil.copytree('mirrors/', '/etc/dynamite/mirrors')
     except IOError as e:
         sys.stderr.write('[-] An error occurred while attempting to extract file. [{}]\n'.format(e))
