9fb062854600d246b22f358ff2627c18fee0589c
jaminbecker <jamin@vlabs.io>
2019-10-10 12:33:36 -0400

    updated b64encode python3 method
    Former-commit-id: 2a1e16a76c69b36192b2af656885ad39026cd8ea

--- dynamite_nsm/services/elasticsearch.py
+++ dynamite_nsm/services/elasticsearch.py
@@ -250,7 +250,7 @@ class ElasticPasswordConfigurator:
             try:
                 base64string = base64.b64encode('%s:%s' % (self.auth_user, self.current_password))
             except TypeError:
-                encoded_bytes = '{}:{}'.format(self.auth_user, self.current_password).encode('utf-8')
+                encoded_bytes = base64.b64encode(b'%s:%s' % (self.auth_user, self.current_password))
                 base64string = base64.b64encode(encoded_bytes)
             url_request = Request(
                 url='http://{}:{}/_xpack/security/user/{}/_password'.format(
