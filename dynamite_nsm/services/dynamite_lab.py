cbd77d85f9fe00e08a530d1117045088d217f57d
jaminbecker <jamin@vlabs.io>
2019-11-27 18:09:49 -0500

    added set ownership line

--- dynamite_nsm/services/dynamite_lab.py
+++ dynamite_nsm/services/dynamite_lab.py
@@ -121,7 +121,7 @@ class DynamiteLabInstaller:
         subprocess.call('mkdir -p {}'.format(self.notebook_home), shell=True)
         sdk_install_cache = os.path.join(const.INSTALL_CACHE, const.DYNAMITE_SDK_DIRECTORY_NAME)
         utilities.copytree(os.path.join(sdk_install_cache, 'dynamite_sdk', 'notebooks'), self.notebook_home)
-        # utilities.set_ownership_of_file(self.sdk_home, user='jupyter', group='dynamite')
+        utilities.set_ownership_of_file(self.notebook_home, user='jupyter', group='dynamite')
         p = subprocess.Popen(['python3', 'setup.py', 'install'], cwd=os.path.join(sdk_install_cache, 'dynamite_sdk'))
         p.communicate()
 
