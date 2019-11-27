6f6f52c026fbc458472a002a2be40026a8c2e479
jaminbecker <jamin@vlabs.io>
2019-11-27 18:17:35 -0500

    updated install path

--- dynamite_nsm/services/dynamite_lab.py
+++ dynamite_nsm/services/dynamite_lab.py
@@ -122,7 +122,7 @@ class DynamiteLabInstaller:
         sdk_install_cache = os.path.join(const.INSTALL_CACHE, const.DYNAMITE_SDK_DIRECTORY_NAME)
         utilities.copytree(os.path.join(sdk_install_cache, 'notebooks'), self.notebook_home)
         utilities.set_ownership_of_file(self.notebook_home, user='jupyter', group='dynamite')
-        p = subprocess.Popen(['python3', 'setup.py', 'install'], cwd=os.path.join(sdk_install_cache, 'dynamite_sdk'))
+        p = subprocess.Popen(['python3', 'setup.py', 'install'], cwd=sdk_install_cache)
         p.communicate()
 
     def setup_jupyterhub(self, jupyter_password='changeme'):
