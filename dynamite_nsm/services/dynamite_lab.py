12d003d2a45f61e0892d165880eab67c4af7b3e1
jaminbecker <jamin@vlabs.io>
2019-11-28 14:43:18 -0500

    added stdout stream as well

--- dynamite_nsm/services/dynamite_lab.py
+++ dynamite_nsm/services/dynamite_lab.py
@@ -377,8 +377,8 @@ class JupyterHubProcess:
         """
         def start_shell_out():
             subprocess.call('jupyterhub -f {}'.format(
-                os.path.join(self.configuration_directory, 'jupyterhub_config.py')), shell=True, stdout=subprocess.PIPE,
-                stdin=subprocess.PIPE, close_fds=True)
+                os.path.join(self.configuration_directory, 'jupyterhub_config.py')), shell=True, stderr=subprocess.PIPE,
+                stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
         if not os.path.exists('/var/run/dynamite/jupyterhub/'):
             subprocess.call('mkdir -p {}'.format('/var/run/dynamite/jupyterhub/'), shell=True)
