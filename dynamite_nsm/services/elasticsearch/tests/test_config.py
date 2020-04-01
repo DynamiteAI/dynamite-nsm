3ac1d4be6dfb8c9185ddbbe26267cc27f350ab01
jaminbecker <jamin@vlabs.io>
2020-04-01 17:52:17 -0400

    updated unittest

--- dynamite_nsm/services/elasticsearch/tests/test_config.py
+++ dynamite_nsm/services/elasticsearch/tests/test_config.py
@@ -1,13 +1,21 @@
+import os
 import shutil
 import unittest
 
+from dynamite_nsm import const
+from dynamite_nsm import utilities
 from dynamite_nsm.services.elasticsearch import config
 
 
 class Tests(unittest.TestCase):
 
     def setUp(self):
-        self.config_directory = '/etc/dynamite/elasticsearch'
+        self.config_path = '/etc/dynamite/test'
+        self.config_directory = os.path.join(self.config_path, 'elasticsearch')
+        utilities.download_file(const.DEFAULT_CONFIGS_URL,
+                      const.DEFAULT_CONFIGS_ARCHIVE_NAME, stdout=True)
+        utilities.extract_archive(os.path.join(const.INSTALL_CACHE, 'default_configs.tar.gz'), self.config_path)
+
         self.config_manager = config.ConfigManager(configuration_directory=self.config_directory)
 
     def test_elasticyaml_update(self):
