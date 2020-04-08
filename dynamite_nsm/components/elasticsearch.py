04bb6c2c8be8ad1eb5c16948b7a341f726a2edda
jaminbecker <jamin@vlabs.io>
2020-04-08 16:55:51 -0400

    added uninstall strategy

--- dynamite_nsm/components/elasticsearch.py
+++ dynamite_nsm/components/elasticsearch.py
@@ -4,8 +4,8 @@ from dynamite_nsm.services.elasticsearch import install, process
 
 class ElasticsearchInstallStrategy(exec_strategy.BaseExecStrategy):
 
-    def __init__(self, password, heap_size_gigs, stdout, verbose):
-        exec_strategy.BaseExecStrategy.__init__(self, strategy_name="Elasticsearch Install",
+    def __init__(self, password, heap_size_gigs, install_jdk, stdout, verbose):
+        exec_strategy.BaseExecStrategy.__init__(self, strategy_name="elasticsearch_install",
                                                 strategy_description="Install and secure Elasticsearch.")
         self.add_function(
             install.install_elasticsearch, {
@@ -14,22 +14,47 @@ class ElasticsearchInstallStrategy(exec_strategy.BaseExecStrategy):
                 "log_directory": "/var/log/dynamite/elasticsearch/",
                 "password": str(password),
                 "heap_size_gigs": int(heap_size_gigs),
-                "install_jdk": False,
+                "install_jdk": bool(install_jdk),
                 "create_dynamite_user": True,
                 "stdout": bool(stdout),
                 "verbose": bool(verbose)
-
             }
         )
 
 
-if __name__ == '__main__':
+class ElasticsearchUninstallStrategy(exec_strategy.BaseExecStrategy):
+
+    def __init__(self, stdout, verbose):
+        exec_strategy.BaseExecStrategy.__init__(self, strategy_name="elasticsearch_uninstall",
+                                                strategy_description="Uninstall Elasticsearch.")
+        self.add_function(
+            install.uninstall_elasticsearch(), {
+                "stdout": bool(stdout),
+                "verbose": bool(verbose)
+            }
+        )
+
+
+def run_install_strategy():
     es_elastic_install_strategy = ElasticsearchInstallStrategy(
         password="changeme",
         heap_size_gigs=4,
+        install_jdk=False,
         stdout=True,
         verbose=True
     )
-
     es_elastic_install_strategy.execute_strategy()
 
+
+def run_uninstall_strategy():
+    es_elastic_uninstall_strategy = ElasticsearchUninstallStrategy(
+        stdout=True,
+        verbose=True
+    )
+    es_elastic_uninstall_strategy.execute_strategy()
+
+
+if __name__ == '__main__':
+    # run_install_strategy()
+    run_uninstall_strategy()
+    pass
