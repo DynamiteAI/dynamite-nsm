442ee618db2774b1bbf9a7caaca5ab90306aff49
jaminbecker <jamin@vlabs.io>
2020-04-22 17:42:54 -0400

    updated characters

--- dynamite_nsm/utilities.py
+++ dynamite_nsm/utilities.py
@@ -422,7 +422,7 @@ def run_subprocess_with_status(process, expected_lines=None):
     widgets = [
         '[+] ', progressbar.Percentage(),
         ' ', progressbar.Bar(),
-        ' ', progressbar.FormatLabel('[?]'),
+        ' ', progressbar.FormatLabel('|?|'),
         ' ', progressbar.ETA()
     ]
     over_max_value = False
@@ -440,7 +440,7 @@ def run_subprocess_with_status(process, expected_lines=None):
             i += 1
             try:
                 if not over_max_value:
-                    widgets[5] = progressbar.FormatLabel('[{}...]'.format(output[0:25]))
+                    widgets[5] = progressbar.FormatLabel('|{}...|'.format(output[0:25]))
                     pb.update(i)
             except ValueError:
                 if not over_max_value:
