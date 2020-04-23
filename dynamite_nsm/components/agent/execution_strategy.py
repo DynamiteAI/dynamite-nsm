01c3ee56b8b3c1e3d111fce68b11ae60c915a7de
jaminbecker <jamin@vlabs.io>
2020-04-22 21:36:14 -0400

    fixed padding

--- dynamite_nsm/components/agent/execution_strategy.py
+++ dynamite_nsm/components/agent/execution_strategy.py
@@ -71,7 +71,7 @@ def log_message(msg, level=logging.INFO, stdout=True, verbose=False):
     log_level = logging.INFO
     if verbose:
         log_level = logging.DEBUG
-    logger = get_logger('AGENT_COMPONENT', level=log_level, stdout=stdout)
+    logger = get_logger('AGENT_CMP', level=log_level, stdout=stdout)
     if level == logging.DEBUG:
         logger.debug(msg)
     elif level == logging.INFO:
--- dynamite_nsm/logger.py
+++ dynamite_nsm/logger.py
@@ -19,5 +19,5 @@ def get_logger(component_name, level=logging.INFO, stdout=True):
     logger.addHandler(fh)
     if stdout:
         coloredlogs.install(level=level, logger=logger,
-                            fmt='[%(asctime)s],[%(name)10s] [%(levelname)8s] -- %(message)s')
+                            fmt='%(asctime)s %(name)-12s [%(levelname)-8s] -- %(message)s')
     return logger
