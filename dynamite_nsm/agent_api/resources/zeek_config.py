9c848ec78b96cfe2bcd18fde7061a339571c23b6
jaminbecker <jamin@vlabs.io>
2020-05-28 12:52:42 -0400

    moved to jsonify

--- dynamite_nsm/agent_api/resources/zeek_config.py
+++ dynamite_nsm/agent_api/resources/zeek_config.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from flask_restful import Resource
 
 from dynamite_nsm import utilities
@@ -9,32 +10,6 @@ ZEEK_INSTALL_DIRECTORY = env_vars.get('ZEEK_HOME')
 
 def err_msg(msg):
     return {'error': msg}
-
-
-class ZeekNodeConfig(Resource):
-
-    def __init__(self):
-        self.node_config = zeek_config.NodeConfigManager(install_directory=ZEEK_INSTALL_DIRECTORY)
-
-    def get(self, component='all'):
-        manager = self.node_config.get_manager()
-        loggers = self.node_config.list_loggers()
-        proxies = self.node_config.list_proxies()
-        workers = self.node_config.list_workers()
-        components = dict(
-            manager=manager,
-            loggers=loggers,
-            proxies=proxies,
-            workers=workers
-        )
-        if component == 'all':
-            return components, 200
-        else:
-            try:
-                return dict(component=components[component]), 200
-            except KeyError:
-                return err_msg(
-                    "Invalid component valid components are ['manager', 'loggers', 'proxies', 'workers', 'all']"), 400
 
 
 class ZeekNodeComponentsList(Resource):
@@ -53,5 +28,28 @@ class ZeekNodeComponentsList(Resource):
             proxies=proxies,
             workers=workers
         )
-        return dict(components=components), 200
+        return jsonify(components=components), 200
 
+
+class ZeekNodeConfig(Resource):
+
+    def __init__(self):
+        self.node_config = zeek_config.NodeConfigManager(install_directory=ZEEK_INSTALL_DIRECTORY)
+
+    def get(self, component):
+        manager = self.node_config.get_manager()
+        loggers = self.node_config.list_loggers()
+        proxies = self.node_config.list_proxies()
+        workers = self.node_config.list_workers()
+        components = dict(
+            manager=manager,
+            loggers=loggers,
+            proxies=proxies,
+            workers=workers
+        )
+        try:
+            return jsonify(component=components[component]), 200
+        except KeyError:
+            return jsonify(
+                error="Invalid component valid components are "
+                      "['manager', 'loggers', 'proxies', 'workers']"), 400
