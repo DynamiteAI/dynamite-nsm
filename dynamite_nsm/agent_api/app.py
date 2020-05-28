d997328768dedafe3cfddc25f3e02a910a04a378
jaminbecker <jamin@vlabs.io>
2020-05-28 12:45:49 -0400

    added component arg

--- dynamite_nsm/agent_api/app.py
+++ dynamite_nsm/agent_api/app.py
@@ -5,7 +5,7 @@ from dynamite_nsm.agent_api.resources import zeek_config
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(zeek_config.ZeekNodeComponentsList, '/zeek/config/node')
+api.add_resource(zeek_config.ZeekNodeComponentsList, '/zeek/config/node/')
 api.add_resource(zeek_config.ZeekNodeConfig, '/zeek/config/node/<string:component>')
 
 if __name__ == '__main__':
