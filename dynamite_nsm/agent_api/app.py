0dfbe7f973799ecb8d932226eb48eed7cb013a7e
jaminbecker <jamin@vlabs.io>
2020-05-28 13:08:03 -0400

    added some basic system info methos

--- dynamite_nsm/agent_api/app.py
+++ dynamite_nsm/agent_api/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_restful import Api
+from dynamite_nsm.agent_api.resources import system_info
 from dynamite_nsm.agent_api.resources import zeek_config
 
 app = Flask(__name__)
 api = Api(app)
+
+api.add_resource(system_info.SystemInfo, '/system/')
+api.add_resource(system_info.CPUCoreCount, '/system/cpus/')
+api.add_resource(system_info.MemoryBytes, '/system/memory/')
+api.add_resource(system_info.NetworkAddresses, '/system/ips/')
+api.add_resource(system_info.NetworkInterfaces, '/system/interfaces/')
 
 api.add_resource(zeek_config.ZeekNodeComponentsList, '/zeek/config/node/')
 api.add_resource(zeek_config.ZeekNodeConfig, '/zeek/config/node/<string:component>')
---
+++ dynamite_nsm/agent_api/resources/system_info.py
@@ -0,0 +1,38 @@
+from flask_restful import Resource
+
+from dynamite_nsm import utilities
+
+
+class CPUCoreCount(Resource):
+
+    def get(self):
+        return dict(cpu_core_count=utilities.get_cpu_core_count()), 200
+
+
+class MemoryBytes(Resource):
+
+    def get(self):
+        return dict(memory_bytes=utilities.get_memory_available_bytes()), 200
+
+
+class NetworkAddresses(Resource):
+
+    def get(self):
+        return dict(ip_addresses=utilities.get_network_addresses()), 200
+
+
+class NetworkInterfaces(Resource):
+
+    def get(self):
+        return dict(interfaces=utilities.get_network_interface_names()), 200
+
+
+class SystemInfo(Resource):
+
+    def get(self):
+        return dict(
+            memory=utilities.get_memory_available_bytes(),
+            cpu_core_count=utilities.get_cpu_core_count(),
+            addresses=utilities.get_network_addresses(),
+            interfaces=utilities.get_network_interface_names()
+        )
