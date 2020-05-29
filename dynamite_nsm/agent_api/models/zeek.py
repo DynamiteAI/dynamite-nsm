87cb5442d19dd9e2425b1e38ffcb1844e2a8b542
jaminbecker <jamin@vlabs.io>
2020-05-29 11:29:52 -0400

    updated zeek models

--- dynamite_nsm/agent_api/models/zeek.py
+++ dynamite_nsm/agent_api/models/zeek.py
@@ -14,16 +14,43 @@ response_success = dict(
 # GET /config
 response_list_components_model = dict(
     components=dict(
-        manager=fields.Nested,
-        loggers=fields.List(fields.Nested),
-        proxies=fields.List(fields.Nested),
-        workers=fields.List(fields.Nested),
+        manager=fields.Nested(
+            dict(
+                host=fields.String,
+                type=fields.String
+            )
+        ),
+        loggers=fields.List(fields.Nested(
+            dict(
+                host=fields.String,
+                type=fields.String
+            )
+        )),
+        proxies=fields.List(fields.Nested(
+            dict(
+                host=fields.String,
+                type=fields.String
+            )
+        )),
+        workers=fields.List(fields.Nested(
+            dict(
+                type=fields.String,
+                interface=fields.String,
+                lb_method=fields.String,
+                lb_procs=fields.String,
+                pin_cpus=fields.String,
+                host=fields.String
+            )
+        )),
     )
 )
 
 # GET /config/<component>
 response_get_component_model = dict(
-    component=fields.List(fields.Nested)
+    component=fields.List(fields.Nested(dict(
+        host=fields.String,
+        type=fields.String
+    )))
 )
 
 # GET  config/worker/<name>
