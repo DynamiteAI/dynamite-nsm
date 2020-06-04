b456208414eab68c150bd6d6b605400c4028d03f
jaminbecker <jamin@vlabs.io>
2020-06-04 14:21:20 -0400

    added address_group manager

--- dynamite_nsm/agent_api/resources/suricata_config.py
+++ dynamite_nsm/agent_api/resources/suricata_config.py
@@ -1,6 +1,7 @@
 from flask_restplus import fields, reqparse, Namespace, Resource
 
 from dynamite_nsm import utilities
+from dynamite_nsm.agent_api import validators
 from dynamite_nsm.services.suricata import config as suricata_config
 
 api = Namespace(
@@ -10,6 +11,21 @@ api = Namespace(
 
 env_vars = utilities.get_environment_file_dict()
 SURICATA_CONFIG_DIRECTORY = env_vars.get('SURICATA_CONFIG')
+
+model_suricata_address_groups = api.model('SuricataAddressGroupsResponse', model=dict(
+    home_net=fields.String,
+    external_net=fields.String,
+    http_servers=fields.String,
+    sql_servers=fields.String,
+    dns_servers=fields.String,
+    telnet_servers=fields.String,
+    aim_servers=fields.String,
+    domain_controllers=fields.String,
+    modbus_server=fields.String,
+    modbud_client=fields.String,
+    enip_client=fields.String,
+    enip_server=fields.String
+))
 
 
 @api.route('/address-groups', endpoint='suricata-address-groups-config')
@@ -25,7 +41,7 @@ class SuricataAddressGroupsConfig(Resource):
             dns_servers=suricata_instance_config.dns_servers,
             telnet_servers=suricata_instance_config.telnet_servers,
             aim_servers=suricata_instance_config.aim_servers,
-            domain_controllers=suricata_instance_config.dc_servers,
+            dc_servers=suricata_instance_config.dc_servers,
             modbus_server=suricata_instance_config.modbus_server,
             modbud_client=suricata_instance_config.modbus_client,
             enip_client=suricata_instance_config.enip_client,
@@ -33,10 +49,24 @@ class SuricataAddressGroupsConfig(Resource):
         )
 
 
-@api.route('/interfaces', endpoint='suricata-interfaces')
+@api.route('/address-groups/<address_group>', endpoint='suricata-address-groups-manager')
 class SuricataInterfacesConfig(Resource):
 
     def get(self):
         suricata_instance_config = suricata_config.ConfigManager(configuration_directory=SURICATA_CONFIG_DIRECTORY)
-        return dict(interfaces=suricata_instance_config.af_packet_interfaces), 200
+        return suricata_instance_config.af_packet_interfaces, 200
 
+
+@api.route('/address-groups/<address_group>', endpoint='suricata-address-group-manager')
+class SuricataAddressGroupsManager(Resource):
+    VALID_ADDRESS_GROUP_NAMES = ['home_net', 'external_net', 'http_servers', 'sql_servers', 'dns_servers',
+                                 'telnet_servers', 'aim_servers', 'dc_servers', 'modbus_server', 'modbud_client',
+                                 'enip_client', 'enip_server'
+                                 ]
+
+    def get(self, address_group):
+        suricata_instance_config = suricata_config.ConfigManager(configuration_directory=SURICATA_CONFIG_DIRECTORY)
+        if not validators.validate_name(address_group):
+            return dict(message='Invalid "address_group"; must be one of the following : {}'.format(
+                self.VALID_ADDRESS_GROUP_NAMES)), 400
+        return dict(address_group={'name': address_group, 'value': getattr(suricata_instance_config, address_group)})
--- dynamite_nsm/agent_api/validators.py
+++ dynamite_nsm/agent_api/validators.py
@@ -11,3 +11,25 @@ def validate_name(s):
     :return: True, if meets name conditions
     """
     return bool(5 <= len(s) <= 30 and re.search(r'^[a-zA-Z0-9]([\w -]*[a-zA-Z0-9]$)', s))
+
+
+'''
+            home_net=suricata_instance_config.home_net,
+            external_net=suricata_instance_config.external_net,
+            http_servers=suricata_instance_config.http_servers,
+            sql_servers=suricata_instance_config.sql_servers,
+            dns_servers=suricata_instance_config.dns_servers,
+            telnet_servers=suricata_instance_config.telnet_servers,
+            aim_servers=suricata_instance_config.aim_servers,
+            domain_controllers=suricata_instance_config.dc_servers,
+            modbus_server=suricata_instance_config.modbus_server,
+            modbud_client=suricata_instance_config.modbus_client,
+            enip_client=suricata_instance_config.enip_client,
+            enip_server=suricata_instance_config.enip_server
+'''
+
+
+def validate_suricata_address_group_name(s):
+    return s in ['home_net', 'external_net', 'http_servers', 'sql_servers', 'dns_servers', 'telnet_servers',
+                 'aim_servers', 'dc_servers', 'modbus_server', 'modbud_client', 'enip_client', 'enip_server'
+                 ]
