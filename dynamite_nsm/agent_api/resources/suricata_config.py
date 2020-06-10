82ef70ee34806820e0c1d61ea3b1633e3fa7eacc
jaminbecker <jamin@vlabs.io>
2020-06-10 16:06:05 -0400

    simplified readability

--- dynamite_nsm/agent_api/resources/suricata_config.py
+++ dynamite_nsm/agent_api/resources/suricata_config.py
@@ -146,7 +146,9 @@ class SuricataInterfaceManager(Resource):
         if args.interface:
             interface = args.interface
         if args.threads:
-            if not validators.validate_integer(args.threads) or args.threads != 'auto':
+            if args.threads != 'auto':
+                return dict(message="Invalid threads option; valid options are any integer or 'auto' keyword.")
+            elif not validators.validate_integer(args.threads):
                 return dict(message="Invalid threads option; valid options are any integer or 'auto' keyword.")
             threads = int(args.threads)
         if args.cluster_id:
