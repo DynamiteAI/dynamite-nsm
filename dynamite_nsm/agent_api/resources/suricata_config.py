d6c6b603c1259c2859c804296762cad5471ff3a2
jaminbecker <jamin@vlabs.io>
2020-06-10 16:04:47 -0400

    updated conditional check for bad thread val

--- dynamite_nsm/agent_api/resources/suricata_config.py
+++ dynamite_nsm/agent_api/resources/suricata_config.py
@@ -146,7 +146,7 @@ class SuricataInterfaceManager(Resource):
         if args.interface:
             interface = args.interface
         if args.threads:
-            if not validators.validate_integer(args.threads) and args.threads != 'auto':
+            if not validators.validate_integer(args.threads) or args.threads != 'auto':
                 return dict(message="Invalid threads option; valid options are any integer or 'auto' keyword.")
             threads = int(args.threads)
         if args.cluster_id:
