e9e9a641d4a6eed55e75a73dfc9f643b2e8dc441
jaminbecker <jamin@vlabs.io>
2020-06-15 16:38:10 -0400

    updated suricata rules

--- dynamite_nsm/agent_api/resources/suricata_rules.py
+++ dynamite_nsm/agent_api/resources/suricata_rules.py
@@ -31,3 +31,24 @@ class SuricataRuleConfig(Resource):
         rules_and_ids = self.hash_and_id_rules(rules_config.list_enabled_rules(), rules_config.list_disabled_rules())
         return dict(rules=rules_and_ids), 200
 
+
+@api.route('/<rule_id>', endpoint='rule-manager')
+class SuricataRuleManager(Resource):
+
+    def get(self, rule_id):
+        rules_config = suricata_config.ConfigManager(SURICATA_CONFIG_DIRECTORY)
+        rules_and_ids = SuricataRuleConfig.hash_and_id_rules(rules_config.list_enabled_rules(),
+                                                             rules_config.list_disabled_rules())
+        enabled_rules = [str(script['id']) for script in rules_and_ids['enabled']]
+        disabled_rules = [str(script['id']) for script in rules_and_ids['disabled']]
+
+        if rule_id in enabled_rules:
+            idx = enabled_rules.index(rule_id)
+            rules_and_ids['enabled'][idx].update({'status': 'enabled'})
+            return dict(rule=rules_and_ids['enabled'][idx]), 200
+        elif rule_id in disabled_rules:
+            idx = disabled_rules.index(rule_id)
+            rules_and_ids['disabled'][idx].update({'status': 'disabled'})
+            return dict(script=rules_and_ids['disabled'][idx]), 200
+        else:
+            return dict(message='Could not find rule {}'.format(rule_id)), 404
