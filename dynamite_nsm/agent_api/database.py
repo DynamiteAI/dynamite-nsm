911aee420d1f59e02cf00fe8ea62e2455b2d109d
jaminbecker <jamin@vlabs.io>
2020-06-16 16:26:36 -0400

    updated imports

--- dynamite_nsm/agent_api/database.py
+++ dynamite_nsm/agent_api/database.py
@@ -17,5 +17,5 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    import models
+    from dynamite_nsm.agent_api import models
     Base.metadata.create_all(bind=engine)
