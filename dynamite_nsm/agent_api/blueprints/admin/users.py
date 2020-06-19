8c8e49007b30498f9d8775f53aaa42be5641f0e1
jaminbecker <jamin@vlabs.io>
2020-06-18 23:52:15 -0400

    added create

--- dynamite_nsm/agent_api/blueprints/admin/users.py
+++ dynamite_nsm/agent_api/blueprints/admin/users.py
@@ -1,14 +1,17 @@
-from flask import render_template, Blueprint
+from flask import request
 from flask_security import roles_accepted
+from flask import render_template, Blueprint
+from flask_security import SQLAlchemySessionUserDatastore
 
 from dynamite_nsm.agent_api import models
+from dynamite_nsm.agent_api.database import db_session, init_db
 
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
 
 
 @users_blueprint.route('/')
 @roles_accepted('admin')
-def list_users():
+def list_users_html():
     users = models.User.query.all()
     users_list = []
     for user in users:
@@ -25,3 +28,20 @@ def list_users():
             }
         )
     return render_template('admin/users.html', users=users_list)
+
+
+@users_blueprint.route('/create')
+@roles_accepted('admin')
+def create_new_user_html():
+    render_template('admin/create_new_user.html')
+
+
+@users_blueprint.route('/create_new_user')
+@roles_accepted('admin')
+def create_new_user():
+    user_datastore = SQLAlchemySessionUserDatastore(db_session, models.User, models.Role)
+    init_db()
+    email = request.form.email
+    username = request.form.name
+    password = request.form.password
+    user_datastore.create_user(email=email, username=username, password=password)
