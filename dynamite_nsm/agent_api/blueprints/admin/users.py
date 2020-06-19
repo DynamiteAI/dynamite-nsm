2bf38ef4fe258351b34dac23e4de6b86942298be
jaminbecker <jamin@vlabs.io>
2020-06-19 17:37:33 -0400

    added logout and delete logic

--- dynamite_nsm/agent_api/blueprints/admin/users.py
+++ dynamite_nsm/agent_api/blueprints/admin/users.py
@@ -1,6 +1,6 @@
 from flask import request, redirect, url_for
 from flask_security import roles_accepted
-from flask_login import logout_user
+from flask_login import logout_user, current_user
 from flask import render_template, Blueprint
 from flask_security import SQLAlchemySessionUserDatastore
 from sqlalchemy.exc import IntegrityError
@@ -56,11 +56,6 @@ def create_new_user_form():
         if role not in ['admin', 'superuser', 'analyst']:
             redirect(url_for('users.create_user_form_html'))
         elif email == 'admin@dynamite.local':
-
-            user_obj = user_datastore.find_user(email=email)
-            user_datastore.delete_user(user_obj)
-            db_session.commit()
-            logout_user()
             return redirect(url_for('users.create_user_form_html'))
         elif username == 'admin':
             return redirect(url_for('users.create_user_form_html'))
@@ -78,4 +73,8 @@ def create_new_user_form():
             return redirect(url_for('users.create_user_form_html'))
     except KeyError:
         redirect(url_for('users.create_user_form_html'))
-    return redirect('/users')
+    user_obj = user_datastore.find_user(email=current_user.email)
+    user_datastore.delete_user(user_obj)
+    db_session.commit()
+    logout_user()
+    return redirect('/home')
