8e6d621babbacfe0e01d94a9d289555b958d652d
JaminB <jaminbecker@gmail.com>
2020-07-27 13:00:04 -0400

    added back colored logs

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
                 'minimal configuration, and intuitive management.',
     include_package_data=True,
     install_requires=[
+        'coloredlogs',
         'progressbar',
         'pyyaml',
         'npyscreen',
