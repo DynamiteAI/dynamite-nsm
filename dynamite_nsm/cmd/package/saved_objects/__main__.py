e87417058b0ad83d7f87cf45fce25b5a44f9e5a9
JaminB <jaminbecker@gmail.com>
2021-03-26 15:58:40 -0400

    small fix to get the MultipleRespInterface working

--- dynamite_nsm/cmd/package/saved_objects/__main__.py
+++ dynamite_nsm/cmd/package/saved_objects/__main__.py
@@ -5,7 +5,7 @@ from dynamite_nsm.logger import get_logger
 if __name__ == '__main__':
     parser = saved_objects_interface.get_parser()
     args = parser.parse_args()
-    logger = get_logger('Saved Objects', level=DEBUG if args.verbose else INFO, stdout=args.stdout)
+    logger = get_logger('Saved Objects', level=DEBUG if args.verbose else INFO, stdout=True)
     logger.info('Calling package.saved_objects')
     logger.debug(args.__dict__)
     result = saved_objects_interface.execute(args)
