401314e666c0570e45cf898f8da6f9cb1bf1bc06
Louis Monday <louis@dynamite.ai>
2021-04-02 17:00:35 -0700

    Add InstalledPackages/InstalledPackagesSchema, handle error and success output for package installations, track and update which saved objects are installed, and their data required for uninstallation in json file in /etc/dynamite/kibana

--- dynamite_nsm/services/kibana/package.py
+++ dynamite_nsm/services/kibana/package.py
@@ -1,6 +1,7 @@
 import logging
 import mimetypes
 import tarfile
+import os
 import json
 import requests
 from getpass import getpass
@@ -10,7 +11,34 @@ from marshmallow import Schema, fields, validate, ValidationError
 
 
 from dynamite_nsm.logger import get_logger
+from dynamite_nsm.const import INSTALLED_KIBANA_PACKAGES_FILE
 from dynamite_nsm.utilities import get_primary_ip_address
+
+INSTALLED_KIBANA_PACKAGES_FILE_BASE = {'installed_packages': {}}
+
+class InstalledPackagesListSchema(Schema):
+    # should we use sqlite instead?
+    installed_packages = fields.Dict(required=True)
+
+class InstalledPackages(object):
+    def __init__(self, json_data):
+        try:
+            if type(json_data) == dict:
+                self.data = InstalledPackagesListSchema().load(json_data)
+            elif type(json_data) == str:
+                self.data = InstalledPackagesListSchema().loads(json_data)
+            else:
+                raise ValidationError("Invalid input type. must be one of: str, dict")
+        except ValidationError as e:
+            print(e.messages)
+            return None
+        self.installed_packages = self.data.get('installed_packages')
+
+    def json(self) -> str:
+        return json.dumps(self.data)
+
+    def __repr__(self) -> str:
+        return f"<InstalledPackages [{len(self.installed_packages)} installed])>"
 
 
 class PackageManifestSchema(Schema):
@@ -24,7 +52,7 @@ class PackageManifestSchema(Schema):
                             validate=validate.Length(1))
     author_email = fields.String(required=False, default="")
     
-    
+
 
 class PackageManifest(object):
     def __init__(self, json_data):
@@ -45,10 +73,10 @@ class PackageManifest(object):
         for key, value in self.data.items():
             setattr(self, key, value)
     
-    def json(self):
+    def json(self) -> str:
         return json.dumps(self.data)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<PackageManifest(name={self.name}, author={self.author})>"
 
 
@@ -66,11 +94,26 @@ class SavedObjectsManager(object):
         if verbose:
             log_level = logging.DEBUG
         self.logger = get_logger(str('KIBANA.PACKAGE_MANAGER'), level=log_level, stdout=stdout)
+        self.verbose = verbose
+        self._installed_packages = None
 
+    @property
+    def installed_packages(self) -> InstalledPackages:
+        if not self._installed_packages:
+            if not os.path.exists(INSTALLED_KIBANA_PACKAGES_FILE):
+                with open(INSTALLED_KIBANA_PACKAGES_FILE, 'w'):
+                    # open the file as writeable and pass to create it
+                    pass
+            with open(INSTALLED_KIBANA_PACKAGES_FILE, 'r') as ikpf:
+                contents = ikpf.read()
+                if not contents:
+                    contents = INSTALLED_KIBANA_PACKAGES_FILE_BASE
+                self._installed_packages = InstalledPackages(contents)
+        return self._installed_packages
     @property
     def kibana_url(self):
         return f'http://{get_primary_ip_address()}:5601'
-
+        
     @staticmethod
     def _get_kibana_auth_securely(username: Optional[str] = None, password: Optional[str] = None) -> Tuple[str, str]:
         # need to be able to provide these as parameters to the cmd
@@ -81,6 +124,25 @@ class SavedObjectsManager(object):
             print()
             password = getpass("Kibana Password: ")
         return username, password
+
+    def _update_installed_packages_file(self):
+        self._installed_packages_json = self.installed_packages
+        with open(INSTALLED_KIBANA_PACKAGES_FILE, 'w') as packagedata:
+            if self._installed_packages:
+                packagedata.write(self._installed_packages.json())
+    def _process_package_installation_results(self, kibana_response: dict) -> bool:
+        success = kibana_response.get('success', False)
+        errors = kibana_response.get('errors')
+        if errors:
+            for error in errors:
+                print(f"{error['title']} - {error.get('error', {'type': 'unknown'})['type']}")
+                if self.verbose:
+                    print(f"{error}\n")
+        for installed in kibana_response.get('successResults', []):
+            self.installed_packages.installed_packages[installed['id']] = installed
+
+
+        return success
 
     def browse_saved_objects(self, username: Optional[str] = None, password: Optional[str] = None,
                              saved_object_type: Optional[str] = None) -> requests.Response:
@@ -140,31 +202,43 @@ class SavedObjectsManager(object):
         # check mimetype of the file to determine how to proceed
 
         filetype, encoding = mimetypes.MimeTypes().guess_type(package_install_path)
+        installation_statuses = []
+        package_name = "Package"
         if filetype == 'application/x-tar' or encoding == 'gzip':
             # handle tarfile
             tar = tarfile.open(package_install_path)
             try:
                 manifest = tar.extractfile('manifest.json')
-                manifest = PackageManifest(manifest.read())
+                manifest = PackageManifest(manifest.read().decode('utf8'))
+                package_name = manifest.name
             except KeyError:
                 print("Package must contain a manifest.json")
                 exit(0)
             for member in manifest.file_list:
                 # should we validate the json before sending it up to kibana?
-                # TODO: Check folders recursively
                 kibana_objects_file = BytesIO(tar.extractfile(member).read())
-                self.import_kibana_saved_objects(username=username, password=password,
+                result = self.import_kibana_saved_objects(username=username, password=password,
                                                  kibana_objects_file=kibana_objects_file)
                 kibana_objects_file.close()
-
+                installation_statuses.append(self._process_package_installation_results(result))
+        # Should we remove this and ONLY install validatable packages?
         elif filetype in ('application/json', 'text/plain') or any(
                 [package_install_path.endswith('.ndjson'), package_install_path.endswith('json')]):
             with open(package_install_path, 'r') as kibana_objects_file:
-                self.import_kibana_saved_objects(username=username, password=password,
+                result = self.import_kibana_saved_objects(username=username, password=password,
                                                  kibana_objects_file=kibana_objects_file)
+                installation_statuses.append(self._process_package_installation_results(result))
         else:
             self.logger.error("Files must be one of: .ndjson, .json, .tar.xz, .tar.gz")
             # TODO raise exception
+        
+        if not all(installation_statuses):
+            print(f"\r\n{package_name} installation failed.")
+            if not self.verbose:
+                print("Use --verbose flag to see more error detail.\n")
+        else:
+            print(f"\r\n{package_name} installation succeeded!")
+        self._update_installed_packages_file()
 
     def list(self, username: Optional[str] = None, password: Optional[str] = None):
         """
