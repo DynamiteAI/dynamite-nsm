c83669b7ff0b051c2e67663df0b7244dc34a3ff6
JaminB <jamin@dynamite.ai>
2021-06-16 13:41:34 -0400

    added logic to handle update checking; corrected bug where the url value was getting set to none

--- dynamite_nsm/services/__init__.py
+++ dynamite_nsm/services/__init__.py
@@ -1,6 +1,16 @@
+import os
 from dynamite_nsm import const
 from dynamite_nsm import utilities
 
-utilities.makedirs(const.INSTALL_CACHE, exist_ok=True)
-utilities.makedirs(const.INSTALL_PATH, exist_ok=True)
-utilities.makedirs(const.CONFIG_PATH, exist_ok=True)
+
+def check_updates():
+    utilities.makedirs(const.INSTALL_CACHE, exist_ok=True)
+    utilities.makedirs(const.INSTALL_PATH, exist_ok=True)
+    utilities.makedirs(const.CONFIG_PATH, exist_ok=True)
+    if not os.path.exists(const.DEFAULT_CONFIGS):
+        from dynamite_nsm.services.updates import install as update_installer
+        update_mng = update_installer.InstallManager(stdout=True)
+        update_mng.setup()
+
+
+check_updates()
