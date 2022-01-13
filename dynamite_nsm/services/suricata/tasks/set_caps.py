2788d9c646acbca9911919db0f792a0527de7954
JaminB <jamin@dynamite.ai>
2022-01-12 19:20:18 -0500

    set setcap abs path

--- dynamite_nsm/services/suricata/tasks/set_caps.py
+++ dynamite_nsm/services/suricata/tasks/set_caps.py
@@ -7,7 +7,7 @@ class SetCapturePermissions(tasks.BaseShellCommandsTask):
 
     def __init__(self, suricata_install_directory: str):
         super().__init__(name='set_zeek_capture_permissions', package_link='N/A', commands=[
-            ['setcap', 'cap_net_raw,cap_net_admin=eip', f'{suricata_install_directory}/bin/suricata'],
+            ['/usr/sbin/setcap', 'cap_net_raw,cap_net_admin=eip', f'{suricata_install_directory}/bin/suricata'],
         ])
 
     def invoke(self, shell: Optional[bool] = False, cwd: Optional[str] = os.getcwd()) -> List[Tuple[List, bytes, bytes]]:
--- dynamite_nsm/services/zeek/tasks/set_caps.py
+++ dynamite_nsm/services/zeek/tasks/set_caps.py
@@ -7,8 +7,8 @@ class SetCapturePermissions(tasks.BaseShellCommandsTask):
 
     def __init__(self, zeek_install_directory: str):
         super().__init__(name='set_zeek_capture_permissions', package_link='N/A', commands=[
-            ['setcap', 'cap_net_raw=eip', f'{zeek_install_directory}/bin/zeek'],
-            ['setcap', 'cap_net_raw=eip', f'{zeek_install_directory}/bin/capstats'],
+            ['/usr/sbin/setcap', 'cap_net_raw=eip', f'{zeek_install_directory}/bin/zeek'],
+            ['/usr/sbin/setcap', 'cap_net_raw=eip', f'{zeek_install_directory}/bin/capstats'],
         ])
 
     def invoke(self, shell: Optional[bool] = False, cwd: Optional[str] = os.getcwd()) -> List[Tuple[List, bytes, bytes]]:
